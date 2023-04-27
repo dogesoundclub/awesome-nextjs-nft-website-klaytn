a492c076bfb85a7dd7ab4906f71f341a26b0aa6f
seongmoolee <seongmooleekr@gmail.com>
2023-04-27 15:00:50 +0900

    v6 rewrite

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
           url: 'https://dogesound.club/',
           title: 'DogeSoundClub - NFT를 수집하는 유쾌한 사람들의 모임',
           description: '도지사운드클럽은 NFT를 수집하는 유쾌한 사람들의 모임입니다. 한국 최초이자 클레이튼 최초로 발행된 제너레이티브 NFT인 메이트를 수집하는 것을 시작으로 2021년 7월에 탄생하였습니다.',
-          images: [{ url: 'https://dogesound.club/thumb.jpg' }],
+          images: [{ url: 'https://dogesound.club/thumb.webp' }],
           siteName: 'DogeSoundClub',
         }}
         twitter={{
--- pages/desktop/faq.tsx
+++ pages/desktop/faq.tsx
@@ -8,7 +8,71 @@ export default function Faq() {
             </div>
             <div style={{ overflow:"scroll", height: "60vh", padding: "20px", fontFamily: "Alegreya Sans", fontSize: "20px" }}>
                 <div style={{ color: "#36B167", fontFamily: "Sunflower", fontSize: "32px", textDecoration: "underline" }}>FAQ</div>
-
+                <div>
+                    <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <div>
+                            <img src="/desktop/faq/qna1.webp" alt="" style={{ width: "50px" }} />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", paddingLeft: "10px" }}>
+                            <span style={{ fontSize: "25px" }}>What is DogeSoundClub?</span>
+                            <span style={{ color: "#777777", fontSize: "19px" }}>DogeSoundClub (DSC) is the first online PFP NFT project based in South Korea, based on the Klaytn blockchain.</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <div>
+                            <img src="/desktop/faq/qna2.webp" alt="" style={{ width: "50px" }} />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", paddingLeft: "10px" }}>
+                            <span style={{ fontSize: "25px" }}>Why is this project called DogeSoundClub (DSC)? </span>
+                            <span style={{ color: "#777777", fontSize: "19px" }}>DogeSoundClub, as its name presents, means ‘a club for nonsense bullshiters’. Anyone with an entrance ticket can come in and freely express various thoughts, even if they are full of nonsense. Mate is a 10,000 pieces of Generative Art NFT which works as a ticket to enter the DogeSoundClub.</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <div>
+                            <img src="/desktop/faq/qna3.webp" alt="" style={{ width: "50px" }} />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", paddingLeft: "10px" }}>
+                            <span style={{ fontSize: "25px" }}>How can I get Mates? </span>
+                            <span style={{ color: "#777777", fontSize: "19px" }}>You can buy our NFTs through the secondary market of OpenSea and also at KLUBS, a marketplace that uses MIX as its currency. </span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <div>
+                            <img src="/desktop/faq/qna4.webp" alt="" style={{ width: "50px" }} />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", paddingLeft: "10px" }}>
+                            <span style={{ fontSize: "25px" }}>What is DogeSoundClub?</span>
+                            <span style={{ color: "#777777", fontSize: "19px" }}>DogeSoundClub (DSC) is the first online PFP NFT project based in South Korea, based on the Klaytn blockchain.</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <div>
+                            <img src="/desktop/faq/qna5.webp" alt="" style={{ width: "50px" }} />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", paddingLeft: "10px" }}>
+                            <span style={{ fontSize: "25px" }}>What is MIX token? </span>
+                            <span style={{ color: "#777777", fontSize: "19px" }}>MIX Token is an cryptocurrency connected to DodgeSoundClub, officially launched by DSC. The MIX token acts as a key currency within the DodgeSoundClub ecosystem. If you have a Mate, you can mine a certain amount of MIX tokens.</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <div>
+                            <img src="/desktop/faq/qna6.webp" alt="" style={{ width: "50px" }} />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", paddingLeft: "10px" }}>
+                            <span style={{ fontSize: "25px" }}>Where can I buy MIX token?</span>
+                            <span style={{ color: "#777777", fontSize: "19px" }}>You can buy MIX token via KLAYswap and KlayFi.</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex", paddingTop: "20px" }}>
+                        <div>
+                            <img src="/desktop/faq/qna7.webp" alt="" style={{ width: "50px" }} />
+                        </div>
+                        <div style={{ display: "flex", flexDirection: "column", paddingLeft: "10px" }}>
+                            <span style={{ fontSize: "25px" }}>What is AYIAS?</span>
+                            <span style={{ color: "#777777", fontSize: "19px" }}>AYIAS is our own metaverse platform. And it’s the final destination of the DogeSoundClub’s ecosystem.</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </Layout>
     );
